974b1d8f1ef74effe4dc86fad8c7347cb3a72d94
lfsotoc <lfsotoc@unal.edu.co>
2024-04-29 08:10:06 -0500

    intento

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,4 +43,11 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
+-- SELECT K0, avg(c21) FROM tbl1, tbl2 t2 WHERE t1.K1 = t2.K1 AND c13 > 400 GROUP BY K0
 
+SELECT K0, avg(c21) 
+FROM tbl1
+INNER JOIN tbl2 ON tbl1.K1 = tbl2.K1
+WHERE c13 > 400
+GROUP BY K0
+ORDER BY K0;
